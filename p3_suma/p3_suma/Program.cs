43df5b3092cb30d787caaa7cad4d90fc0bd73ed9
HugoConde <hugo_conde93@hotmail.com>
2014-09-24 02:38:35 -0500

    Pedir datos de entrada

--- p3_suma/p3_suma/Program.cs
+++ p3_suma/p3_suma/Program.cs
@@ -1,25 +1,32 @@
-﻿/*
- * Created by SharpDevelop.
- * User: Usuario
- * Date: 24/09/2014
- * Time: 02:16 a. m.
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
-using System;
-
-namespace p3_suma
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace ConsoleApplication1
 {
-	class Program
-	{
-		public static void Main(string[] args)
-		{
-			Console.WriteLine("Hello World!");
-			
-			// TODO: Implement Functionality Here
-			
-			Console.Write("Press any key to continue . . . ");
-			Console.ReadKey(true);
-		}
-	}
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int Entero1, Entero2, Entero3; //Declaramos enteros
+            string Numero1, Numero2; //Declaramos Strings;
+            // Ingresamos el valor 1
+            Console.Write("Ingrese un Numero: "); 
+            Numero1 = Console.ReadLine();
+            // Ingresamos el valor 2
+            Console.Write("Ingrese otro: "); 
+            Numero2 = Console.ReadLine();
+            //Convertimos String a Decimal
+            Entero1 = Int32.Parse(Numero1); 
+            Entero2 = Int32.Parse(Numero2);
+            //Sumamos
+            Entero3 = Entero1 + Entero2;
+            Console.Write("El resultado de la suma es: {0}", Entero3);
+            
+        }
+        
+    }
 }
+
+
