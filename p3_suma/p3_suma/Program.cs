d01058f9983cd5698e05301eaf443d382d4aa739
HugoConde <hugo_conde93@hotmail.com>
2014-09-24 02:27:05 -0500

    Base del proyecto

---
+++ p3_suma/p3_suma/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Usuario
+ * Date: 24/09/2014
+ * Time: 02:16 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace p3_suma
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
